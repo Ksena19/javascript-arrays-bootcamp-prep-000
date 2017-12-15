55a0917769a0613320d2e48b3ab86bc87a32b630
Ksena <oksishukh@yahoo.com>
2017-12-15 16:24:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
-var chocolateBars = [ 'snickers', 'hundred grand', 'kitkat', 'skittles' ]
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+function addElementToBeginningOfArray(arr, element) {
+    return [element, ...arr]
+  }
+
+function destructivelyAddElementToBeginningOfArray(arr, element){
+  arr.unshift(element);
+  return arr;
+}
