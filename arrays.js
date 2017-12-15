d4327d1883cd7a1bb85d6ee10d57f86ed7f9d042
Ksena <oksishukh@yahoo.com>
2017-12-15 16:31:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,12 @@ function destructivelyAddElementToBeginningOfArray(arr, element){
   arr.unshift(element);
   return arr;
 }
+
+function addElementToEndOfArray(arr, element) {
+  return [ ...arr, element]
+}
+
+function destructivelyAddElementToEndOfArray(arr, element) {
+  arr.push(element);
+  return arr;
+}
